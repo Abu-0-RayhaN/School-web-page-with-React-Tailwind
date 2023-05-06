3aa6dfcb8d51a1a2da741ff7eb26911af1904770
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-05-06 23:25:36 +0600

    navbar

---
+++ src/assets/index.jsx
@@ -0,0 +1,25 @@
+import banner from './banner.jpg';
+import carousel1 from './carousel1.jpg';
+import carousel2 from './carousel2.jpg';
+import carousel3 from './carousel3.jpg';
+import instruction from './instruction.jpg';
+import leadership from './leadership.jpg';
+import resource from './resource.jpg';
+import nav from './nav.png';
+import hero from './hero.png';
+import equal from './equal.png';
+import cross from './cross.png';
+
+export {
+    banner,
+    carousel1,
+    carousel2,
+    carousel3,
+    instruction,
+    leadership,
+    resource,
+    nav as logo,
+    hero,
+    equal,
+    cross
+}
