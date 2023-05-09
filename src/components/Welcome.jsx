ce9f3f984fa3701f2385d9075ec9764fa1a36887
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-05-09 17:10:23 +0600

    daisyUI setup

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,9 @@
 // import React from "react";
 
 const Welcome = () => {
-  return <div>hello from Welcome</div>;
+  return <div>hello from Welcome
+    <div className="btn">Click to enter a new world</div>
+  </div>;
 };
 
 export default Welcome;
